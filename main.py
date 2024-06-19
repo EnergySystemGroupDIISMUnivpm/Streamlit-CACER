17286c6e059db979fe7a8345722e1749b964efae
Raffy <raffaella.assogna@revoltsrl.com>
2024-06-19 17:16:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#TEST STREAMLIT: SELEZIONARE IL TIPO DI UTENTE E STAMPARLO
+
+import streamlit as st
+
+st.title("ENEA-CER")
+utenti=["Cittadino","Amministratore di condominio","Amministrazione pubblica"]
+utente=st.selectbox("Seleziona il tipo di utente",utenti,index=None)
+if utente!=None:
+ st.write(f"""L'utente selezionato è: {utente}""")

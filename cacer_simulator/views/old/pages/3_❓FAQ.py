6d62a58e43d2ed1cd64c6ffa08683171fd75a71b
Raffy <raffaella.assogna@revoltsrl.com>
2024-09-05 12:37:45 +0200

    Deleted unused files

--- cacer_simulator/views/old/pages/3_❓FAQ.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# CONFIGURATION OF STREAMLIT PAGE
-st.set_page_config(
-    page_title="ENEA Simulatore CACER",
-    page_icon="🌤️",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
--- deprecated/streamlit_app.py
+++
@@ -1,8 +0,0 @@
-# TEST STREAMLIT: SELEZIONARE IL TIPO DI UTENTE E STAMPARLO
-import streamlit as st
-
-st.title("ENEA-CER")
-utenti = ["Cittadino", "Amministratore di condominio", "Amministrazione pubblica"]
-utente = st.selectbox("Seleziona il tipo di utente", utenti, index=None)
-if utente != None:
-    st.write(f"""L'utente selezionato è: {utente}""")

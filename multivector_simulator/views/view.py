1039d08bdd2cb6a0e104eada0320ff56d2b1a142
Raffy <raffaella.assogna@revoltsrl.com>
2025-02-13 16:36:40 +0100

    remove ipdb

--- multivector_simulator/views/view.py
+++ multivector_simulator/views/view.py
@@ -248,7 +248,6 @@ class UserOuput(BaseModel):
             help="Questo grafico mostra l'andamento cumulativo di costi e risparmi annuali, partendo dal costo iniziale dell'installazione  degli impianti e includendo i costi e i risparmi accumulati ogni anno. I valori negativi rappresentano le spese, mentre i valori positivi indicano i risparmi ottenuti. Il punto in cui la barra cumulativa supera lo zero rappresenta il momento in cui si ottiene il ritorno dell'investimento, segnando il passaggio da una situazione di costo netto a una di guadagno.",
         )
         years = common.Optimizer().YEARS
-        __import__("ipdb").set_trace()
         st.bar_chart(
             df_cumulative_costs_and_savings.set_index("Year")["Cumulative value"],
             x_label="Anni dopo l'installazione degli impianti",
